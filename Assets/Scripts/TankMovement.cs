8d1a4fe6b3135913f3ffef50c33b4e3a008d96d9
Jack Kinsey <jack.w.kinsey@gmail.com>
2017-11-15 05:10:36 -0600

    initial commit, testing out various control schemes for spaceship controls

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _thrustSpeed = 10f;
+
+    [SerializeField]
+    private float _rotationSpeed = 3f;
+
+    private Rigidbody2D _rb;
+
+    private float _yAxisInput;
+    private float _xAxisInput;
+
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _xAxisInput = Input.GetAxis("Horizontal");
+        _yAxisInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, 0, _xAxisInput * -_rotationSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        if (_yAxisInput != 0)
+        {
+            _rb.velocity = transform.up * _thrustSpeed;
+            //_rb.AddForce(transform.up * _thrustSpeed);
+        } else
+        {
+            _rb.velocity = new Vector2(0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/TwinStickMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwinStickMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10f;
+
+    private Rigidbody2D _rb;
+
+    private float _xAxisInput;
+    private float _yAxisInput;
+
+    private Vector2 _movement;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _xAxisInput = Input.GetAxis("Horizontal");
+        _yAxisInput = Input.GetAxis("Vertical");
+
+        _movement = new Vector2(_xAxisInput, _yAxisInput);
+
+        if (_movement.magnitude > 1)
+        {
+            _movement.Normalize();
+        }
+
+        if (_movement != Vector2.zero)
+        {
+            // My trig is rusty so I'm not sure what this math is doing really...I'd like to actually understand this code though.
+            // Subtract 90 degrees since the art asset is pointing up and not to the right.
+            float angle = Mathf.Atan2(_movement.y, _movement.x) * Mathf.Rad2Deg - 90;
+
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _movement * _speed;
+    }
+}
