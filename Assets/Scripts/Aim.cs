007ead250f10228f4c901082c14398a690d766e9
Jack Kinsey <jack.w.kinsey@gmail.com>
2017-11-16 18:02:51 -0600

    Implement aim script for mouse and right stick on gamepad

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Aim : MonoBehaviour {
+public class Aim : MonoBehaviour
+{
+    public bool _mouseEnabled = true;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (_mouseEnabled)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePosition - transform.position);
+        } else
+        {
+            float rx = Input.GetAxis("RightHorizontal");
+            float ry = Input.GetAxis("RightVertical");
+
+            Debug.Log(rx + "," + ry);
+
+            if (rx != 0 || ry != 0)
+            {
+                Vector3 axisPosition = new Vector3(rx, ry, 0) + transform.position;
+
+                transform.rotation = Quaternion.LookRotation(Vector3.forward, axisPosition - transform.position);
+            } else
+            {
+                transform.rotation = transform.parent.rotation;
+            }
+        }
+    }
 }
